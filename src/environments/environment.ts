53bd2f48b41d1da878ca4d191cf6248b08502ab1
Mythras <kfarkas86@gmail.com>
2024-04-22 11:36:40 +0200

    env prod not working, trying env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000/api",
+  //apiUrl: "http://localhost:3000/api",
+  apiUrl: "https://hungaryinshadows.vercel.app/api",
   //apiUrl: "http://hungaryinshadows.eu-central-1.elasticbeanstalk.com/",
 };
 
